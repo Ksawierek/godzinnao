63b03c4ef6aa50f79e8f2785e87874de213bdd02
Michał Michaluk <michaluk.michal@gmail.com>
2018-04-05 10:14:08 +0200

    godzinnao init

---
+++ godzinnao-model/src/main/java/io/dddbyexamples/godzinnao/limits/LimitService.java
@@ -0,0 +1,5 @@
+package io.dddbyexamples.godzinnao.limits;
+
+public class LimitService {
+
+}
---
+++ godzinnao-model/src/test/groovy/io/dddbyexamples/godzinnao/limits/LimitSpec.groovy
@@ -0,0 +1,6 @@
+package io.dddbyexamples.godzinnao.limits
+
+import spock.lang.Specification
+
+class LimitSpec extends Specification {
+}
